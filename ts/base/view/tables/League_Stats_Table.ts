93aa8a1f014b1ab9aa186741b1f86ac0382e38d2
Seth Marcus <sem4140@rit.edu>
2019-12-11 16:10:19 -0500

    Removed uneeded html table generating JS

--- ts/base/view/tables/League_Stats_Table.ts
+++ ts/base/view/tables/League_Stats_Table.ts
@@ -1,8 +1,5 @@
 function createLeagueStatsTable(league: League): void {
     initLeagueStatsTable(league);
-    // league.members.forEach((member) => {
-    //     tableBody.appendChild(createLeagueStatsTableRow(league, member));
-    // });
     updateLeagueStatsTable(league);
 
 }
@@ -40,50 +37,6 @@ function getLeagueStatsTableRowData(member: Member): object {
         PP: roundToHundred(member.stats.pp),
         PA: roundToHundred(member.stats.pa)
     };
-}
-
-function createLeagueStatsTableRow(league: League, member: Member): HTMLTableRowElement {
-    const row = document.createElement("tr");
-    const rankCell = document.createElement("td");
-    const teamNameCell = document.createElement("td");
-    const recordCell = document.createElement("td");
-    const pfCell = document.createElement("td");
-    const paCell = document.createElement("td");
-    const ppCell = document.createElement("td");
-    const opslapCell = document.createElement("td");
-    const pctCell = document.createElement("td");
-    const image = document.createElement("img");
-    image.src = member.logoURL;
-    image.style.width = "25px";
-    image.style.height = "25px";
-    image.style.borderRadius = "25px";
-    image.addEventListener("error", fixNoImage);
-    image.style.marginRight = "8px";
-
-    teamNameCell.appendChild(image);
-    teamNameCell.appendChild(document.createTextNode(member.teamNameToString()));
-    rankCell.appendChild(document.createTextNode(member.stats.rank.toString()));
-    pfCell.appendChild(document.createTextNode(roundToHundred(member.stats.pf).toString()));
-    paCell.appendChild(document.createTextNode(roundToHundred(member.stats.pa).toString()));
-    ppCell.appendChild(document.createTextNode(roundToHundred(member.stats.pp).toString()));
-    opslapCell.appendChild(document.createTextNode(roundToHundred(member.stats.OPSLAP).toString()));
-    recordCell.appendChild(document.createTextNode(member.recordToString()));
-    pctCell.appendChild(document.createTextNode(member.stats.getWinPct().toString()));
-    rankCell.style.backgroundColor = getDarkColor(member.stats.rank / league.members.length);
-    pfCell.style.backgroundColor = getDarkColor(league.getPointsScoredFinish(member.teamID) / league.members.length);
-    paCell.style.backgroundColor = getDarkColor(league.getPointsAgainstFinish(member.teamID) / league.members.length);
-    ppCell.style.backgroundColor = getDarkColor(league.getPotentialPointsFinish(member.teamID) / league.members.length);
-    opslapCell.style.backgroundColor = getDarkColor(league.getOPSLAPFinish(member.teamID) / league.members.length);
-    row.appendChild(teamNameCell);
-    row.appendChild(rankCell);
-    row.appendChild(recordCell);
-    row.appendChild(pctCell);
-    row.appendChild(pfCell);
-    row.appendChild(opslapCell);
-    row.appendChild(ppCell);
-    row.appendChild(paCell);
-
-    return row;
 }
 
 function initLeagueStatsTable(league: League) {
--- ts/base/view/tables/Power_Rank_Table.ts
+++ ts/base/view/tables/Power_Rank_Table.ts
@@ -29,47 +29,6 @@ function getPowerRankStatsRowData(member: Member): object {
     };
 }
 
-function createPowerRankTableHtml(league: League) {
-    const tableBody: HTMLTableElement = document.getElementById("power_rank_table_body") as HTMLTableElement;
-    league.members.forEach((member) => {
-        const row = document.createElement("tr");
-        const teamName = document.createElement("td");
-        const powerRank = document.createElement("td");
-        const powerRecord = document.createElement("td");
-        const potentialRecord = document.createElement("td");
-        const actualRank = document.createElement("td");
-        const image = document.createElement("img");
-        const diffRow = document.createElement("td");
-
-        image.src = member.logoURL;
-        image.style.width = "25px";
-        image.style.height = "25px";
-        image.style.borderRadius = "25px";
-        image.addEventListener("error", fixNoImage);
-        image.style.marginRight = "8px";
-
-        teamName.appendChild(image);
-        teamName.appendChild(document.createTextNode(member.teamNameToString()));
-        powerRank.innerText = member.stats.powerRank + "";
-        powerRecord.innerText = member.powerRecordToString();
-        potentialRecord.innerText = member.potentialPowerRecordToString();
-        actualRank.innerText = member.stats.rank + "";
-        const diffText = member.stats.rank - member.stats.powerRank;
-        if (diffText !== 0) {
-            diffRow.style.backgroundColor = getDarkColor(league.getPowerRankDiffFinish(member.teamID) / league.members.length);
-        }
-        actualRank.style.backgroundColor = getDarkColor(member.stats.rank / league.members.length);
-        powerRank.style.backgroundColor = getDarkColor(member.stats.powerRank / league.members.length);
-        diffRow.innerText = (diffText as unknown as string);
-        row.appendChild(teamName);
-        row.appendChild(powerRank);
-        row.appendChild(actualRank);
-        row.appendChild(diffRow);
-        row.appendChild(powerRecord);
-        tableBody.appendChild(row);
-    });
-}
-
 function initPowerRankTable(league: League) {
     $("#power_rank_table").DataTable({
         paging: false,
